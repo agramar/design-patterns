02163f2ac8a29d9bba079a9b1dc49d4c363778d0
Agramar <iamsjh1017@gmail.com>
2018-01-03 01:36:08 +0900

    Iterator Pattern

---
+++ src/kr/co/agramar/iterator/Aggregate.java
@@ -0,0 +1,6 @@
+package kr.co.agramar.iterator;
+
+// 집합체를 나타내는 인터페이스
+public interface Aggregate {
+	public abstract Iterator iterator();
+}
---
+++ src/kr/co/agramar/iterator/Iterator.java
@@ -0,0 +1,10 @@
+package kr.co.agramar.iterator;
+
+// 하나씩 나열하면서 검색을 실행하는 인터페이스
+public interface Iterator {
+	// 다음 요소가 존재하는지 조사
+	public abstract boolean hasNext();
+	
+	// 다음 요소를 얻기 위한 메소드
+	public abstract Object next();
+}
